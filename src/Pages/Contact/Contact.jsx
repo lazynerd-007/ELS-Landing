1b7b12bad920da0ee06e5151837f4c2bf8eadb7b
Stephen Adeniji <praiseadeniji2017@gmail.com>
2023-11-29 14:26:14 +0100

    Feat: implemented 'form submission to email'
    Implemented 'form submission to email', and a toast for error, success and form validation

--- src/Pages/Contact/Contact.jsx
+++ src/Pages/Contact/Contact.jsx
@@ -1,7 +1,10 @@
-import { motion } from "framer-motion";
-import { Link } from "react-router-dom";
+import React, { useState } from 'react';
+import { motion } from 'framer-motion';
+import { Link, useNavigate } from 'react-router-dom';
+import emailjs from '@emailjs/browser';
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 
-// Define animation configuration
 const animationConfiguration = {
     initial: { opacity: 0 },
     animate: { opacity: 1 },
@@ -9,6 +12,70 @@ const animationConfiguration = {
 };
 
 const Contact = () => {
+    const navigate = useNavigate();
+
+    const [values, setValues] = useState({
+        from_name: '',
+        email: '',
+        message: '',
+    });
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+
+        // Check if any of the fields are empty
+        if (!values.from_name || !values.email || !values.message) {
+            // Display an error toast
+            toast.error('Please fill out all fields');
+            return;
+        }
+
+        // Now 'values' contains the current form data
+        console.log('Form values:', values);
+
+        // Perform your emailjs.send() with the 'values' object
+        emailjs.send('service_u6jb8rw', 'template_ffruusu', values, 'MiHjFMb3Hhwh8R9De')
+            // .then((response) => {
+            //     console.log('Email sent successfully:', response);
+            //     // Display a success toast
+            //     toast.success('Message sent successfully, Expect a response within 24hours');
+            // })
+            .then((response) => {
+                console.log('Message sent successfully:', response);
+
+                // Display a success toast with an onClose callback for delayed navigation
+                toast.success('Message sent successfully', {
+                    onClose: () => {
+                        // Introduce a delay (3 seconds) before navigating back to the home page
+                        setTimeout(() => {
+                            navigate('/');
+                        }, 5000);
+                    },
+                });
+            })
+            .catch((error) => {
+                console.error('Error sending email:', error);
+                // Display an error toast
+                toast.error('Error sending email');
+            });
+
+        // Reset the form after submission
+        setValues({
+            from_name: '',
+            email: '',
+            message: '',
+        });
+    };
+
+    const handleInputChange = (e) => {
+        const { name, value } = e.target;
+        setValues({
+            ...values,
+            [name]: value,
+        });
+    };
+
+
     return (
         <motion.div
             variants={animationConfiguration}
@@ -17,6 +84,8 @@ const Contact = () => {
             exit="exit"
             transition={{ duration: 1.5 }}
         >
+            <ToastContainer />
+
             <section className="bg-[#0C0C0C] text-white ">
                 <div className="pt- min-h-screen laptop:px-12 tablet:px-8 px-4">
                     <div className="flex flex-col min-h-screen">
@@ -53,15 +122,20 @@ const Contact = () => {
                                             Ask us anything, we would love to hear from you!
                                         </p>
 
-                                        <form className="mt-6">
+                                        <form onSubmit={handleSubmit}
+                                            className="mt-6">
                                             <div className="flex-1">
                                                 <label className="block mb-2 text-sm text-gray-200">
                                                     Full Name
                                                 </label>
                                                 <input
+                                                    id="from_name"
+                                                    name="from_name"
                                                     type="text"
+                                                    value={values.from_name}
+                                                    onChange={handleInputChange}
                                                     placeholder="John Doe"
-                                                    className="block w-full px-5 py-3 mt-2 text-gray-700 bg-white border border-gray-200 rounded-md focus:ring-opacity-40  focus:outline-none focus:ring"
+                                                    className="block w-full px-5 py-3 mt-2 text-gray-700 bg-white border border-gray-200 rounded-md focus:ring-opacity-40 focus:outline-none focus:ring"
                                                 />
                                             </div>
 
@@ -70,9 +144,13 @@ const Contact = () => {
                                                     Email address
                                                 </label>
                                                 <input
+                                                    id="email"
+                                                    name="email"
                                                     type="email"
+                                                    value={values.email}
+                                                    onChange={handleInputChange}
                                                     placeholder="johndoe@example.com"
-                                                    className="block w-full px-5 py-3 mt-2 text-gray-700 bg-white border border-gray-200 rounded-md focus:ring-opacity-40  focus:outline-none focus:ring"
+                                                    className="block w-full px-5 py-3 mt-2 text-gray-700 bg-white border border-gray-200 rounded-md focus:ring-opacity-40 focus:outline-none focus:ring"
                                                 />
                                             </div>
 
@@ -81,10 +159,30 @@ const Contact = () => {
                                                     Message
                                                 </label>
                                                 <textarea
+                                                    id="message"
+                                                    name="message"
                                                     rows="4"
-                                                    className="block w-full h-32 px-5 py-3 mt-2 text-gray-700 placeholder-gray-400 bg-white border border-gray-200 rounded-md md:h-48  focus:ring-opacity-40  focus:outline-none focus:ring"
+                                                    value={values.message}
+                                                    onChange={handleInputChange}
+                                                    className="block w-full h-32 px-5 py-3 mt-2 text-gray-700 placeholder-gray-400 bg-white border border-gray-200 rounded-md md:h-48 focus:ring-opacity-40 focus:outline-none focus:ring"
                                                     placeholder="Message..."
                                                 ></textarea>
+                                            </div>
+
+                                            <div className="mt-6 flex min-h-[1.5rem] items-center justify-center pl-[1.5rem]">
+                                                <input
+                                                    className="relative float-left -ml-[1.5rem] mr-[6px] h-[1.125rem] w-[1.125rem] rounded-[0.25rem] border-[0.125rem]"
+                                                    // className="relative float-left -ml-[1.5rem] mr-[6px] h-[1.125rem] w-[1.125rem] appearance-none rounded-[0.25rem] border-[0.125rem] border-solid border-neutral-300 outline-none before:pointer-events-none before:absolute before:h-[0.875rem] before:w-[0.875rem] before:scale-0 before:rounded-full before:bg-transparent before:opacity-0 before:shadow-[0px_0px_0px_13px_transparent] before:content-[''] checked:border-primary checked:bg-primary checked:before:opacity-[0.16] checked:after:absolute checked:after:-mt-px checked:after:ml-[0.25rem] checked:after:block checked:after:h-[0.8125rem] checked:after:w-[0.375rem] checked:after:rotate-45 checked:after:border-[0.125rem] checked:after:border-l-0 checked:after:border-t-0 checked:after:border-solid checked:after:border-white checked:after:bg-transparent checked:after:content-[''] hover:cursor-pointer hover:before:opacity-[0.04] hover:before:shadow-[0px_0px_0px_13px_rgba(0,0,0,0.6)] focus:shadow-none focus:transition-[border-color_0.2s] focus:before:scale-100 focus:before:opacity-[0.12] focus:before:shadow-[0px_0px_0px_13px_rgba(0,0,0,0.6)] focus:before:transition-[box-shadow_0.2s,transform_0.2s] focus:after:absolute focus:after:z-[1] focus:after:block focus:after:h-[0.875rem] focus:after:w-[0.875rem] focus:after:rounded-[0.125rem] focus:after:content-[''] checked:focus:before:scale-100 checked:focus:before:shadow-[0px_0px_0px_13px_#3b71ca] checked:focus:before:transition-[box-shadow_0.2s,transform_0.2s] checked:focus:after:-mt-px checked:focus:after:ml-[0.25rem] checked:focus:after:h-[0.8125rem] checked:focus:after:w-[0.375rem] checked:focus:after:rotate-45 checked:focus:after:rounded-none checked:focus:after:border-[0.125rem] checked:focus:after:border-l-0 checked:focus:after:border-t-0 checked:focus:after:border-solid checked:focus:after:border-white checked:focus:after:bg-transparent dark:border-neutral-600 dark:checked:border-primary dark:checked:bg-primary dark:focus:before:shadow-[0px_0px_0px_13px_rgba(255,255,255,0.4)] dark:checked:focus:before:shadow-[0px_0px_0px_13px_#3b71ca]"
+                                                    type="checkbox"
+                                                    value=""
+                                                    id="exampleCheck10"
+                                                />
+                                                <label
+                                                    className="inline-block pl-[0.15rem] hover:cursor-pointer"
+                                                    htmlFor="exampleCheck10"
+                                                >
+                                                    Send me a copy of this message
+                                                </label>
                                             </div>
 
                                             <button type="submit"
