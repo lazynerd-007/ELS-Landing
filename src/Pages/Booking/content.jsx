ebe0bd5484e28bcfde744ab41310f4049b9c0b53
Stephen Adeniji <praiseadeniji2017@gmail.com>
2023-12-05 20:05:53 +0100

    Create content.jsx
    'Times' and 'CarList' arrays for mapping

---
+++ src/Pages/Booking/content.jsx
@@ -0,0 +1,99 @@
+
+export const Times = [
+    '6am', '7am', '8am', '9am', '10am', '11am',
+    '12pm', '1pm', '2pm', '3pm', '4pm', '5pm', '6pm'
+];
+
+export const carList = [
+    {
+        id: 1,
+        name: 'TOYOTA COROLLA 2014 - 2016',
+        rentalAvailable: true,
+        pickupAvailable: true,
+        prices: { rentalWithoutFuel: 29393.41, rentalWithFuel: 39287.66, pickupdropOff: 22858.44, pickupWithFuel: 34287.66 }
+    },
+    {
+        id: 2,
+        name: 'TOYOTA COROLLA 2017 - 2018',
+        rentalAvailable: false,
+        pickupAvailable: true,
+        prices: { rentalWithoutFuel: 150, rentalWithFuel: 200, pickupdropOff: 25205.58, pickupWithFuel: 37808.36 }
+    },
+    {
+        id: 3,
+        name: 'RAV4 2014-2016/ HYUNDAI CRETA',
+        rentalAvailable: true,
+        pickupAvailable: true,
+        prices: { rentalWithoutFuel: 45706.46, rentalWithFuel: 61428.79, pickupdropOff: 37619.19, pickupWithFuel: 56428.79 }
+    },
+    {
+        id: 4,
+        name: 'TOYOTA HIACE 2018',
+        rentalAvailable: false,
+        pickupAvailable: true,
+        prices: { rentalWithoutFuel: 150, rentalWithFuel: 200, pickupdropOff: 44268.79, pickupWithFuel: 66403.18 }
+    },
+    {
+        id: 5,
+        name: 'TOYOTA COASTER',
+        rentalAvailable: false,
+        pickupAvailable: true,
+        prices: { rentalWithoutFuel: 150, rentalWithFuel: 200, pickupdropOff: 67646.77, pickupWithFuel: 101470.16 }
+    },
+    {
+        id: 6,
+        name: 'TOYOTA RAV 4 2019',
+        rentalAvailable: true,
+        pickupAvailable: true,
+        prices: { rentalWithoutFuel: 48713.90, rentalWithFuel: 62032.90, pickupdropOff: 38021.93, pickupWithFuel: 57032.90 }
+    },
+    {
+        id: 7,
+        name: 'TOYOTA PRADO 2014',
+        rentalAvailable: true,
+        pickupAvailable: true,
+        prices: { rentalWithoutFuel: 61091.62, rentalWithFuel: 74410.62, pickupdropOff: 46273.75, pickupWithFuel: 69410.62 }
+    },
+    {
+        id: 8,
+        name: 'TOYOTA PRADO 2018',
+        rentalAvailable: true,
+        pickupAvailable: true,
+        prices: { rentalWithoutFuel: 79955.34, rentalWithFuel: 94986.71, pickupdropOff: 59991.14, pickupWithFuel: 89986.71 }
+    },
+    {
+        id: 9,
+        name: 'TOYOTA LANDCRUISER 2018',
+        rentalAvailable: true,
+        pickupAvailable: true,
+        prices: { pickupdropOff: 73023.38, pickupWithFuel: 109535.07 }
+    },
+    {
+        id: 10,
+        name: 'TOYOTA SIENNA 2015',
+        rentalAvailable: false,
+        pickupAvailable: true,
+        prices: { pickupdropOff: 36666.67, pickupWithFuel: 55000.00 }
+    },
+    {
+        id: 11,
+        name: 'LEXUS GX 460 2015',
+        rentalAvailable: true,
+        pickupAvailable: true,
+        prices: { rentalWithoutFuel: 74965.99, rentalWithFuel: 88284.99, pickupdropOff: 55523.33, pickupWithFuel: 83284.99 }
+    },
+    {
+        id: 12,
+        name: 'TOYOTA FORTUNER 2019',
+        rentalAvailable: true,
+        pickupAvailable: false,
+        prices: { pickupdropOff: 82955.34, pickupWithFuel: 97986.71 }
+    },
+    {
+        id: 13,
+        name: 'TOYOTA PRADO VX 2018',
+        rentalAvailable: true,
+        pickupAvailable: false,
+        prices: { pickupdropOff: 97503.70, pickupWithFuel: 112535.07 }
+    },
+];
