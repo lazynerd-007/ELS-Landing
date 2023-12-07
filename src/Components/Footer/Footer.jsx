1410a600e64faa3e4d64ba870c43a7c742bfa091
Stephen Adeniji <praiseadeniji2017@gmail.com>
2023-12-07 10:20:08 +0100

    feat: update footer content

--- src/Components/Footer/Footer.jsx
+++ src/Components/Footer/Footer.jsx
@@ -1,71 +1,34 @@
-import { FooterData } from "../Navbar/NavbarData";
-import Logo from '../../assets/Logo.png'
+
+
+// date for copyright
+const theDate = new Date().getFullYear();
+// end of date
 
 const Footer = () => {
     return (
         <div>
-            <footer className="bg-[#0C0C0C] text-[#E5E7EB] py-20 border-t border-[#D1D5DB]/40 laptop:px-12 tablet:px-8 px-4 bottom-0">
-                <div className="">
-                    <div className="laptop:flex">
-                        <div className="w-full -mx-6 laptop:w-2/5">
-                            <div className="px-6">
-                                <a href="#">
-                                    <img src={Logo} className='' alt='Logo' />
-                                </a>
-                                <p className="max-w-sm mt-2 text-sm">
-                                    © 2023. All rights reserved
-                                </p>
-
-                            </div>
-                        </div>
-
-                        <div className="mt-6 laptop:mt-0 laptop:flex-1">
-                            <div className="grid grid-cols-2 gap-6 sm:grid-cols-2 tablet:grid-cols-4 laptop:grid-cols-4">
-                                <div className="">
-                                    <h3 className="font-semibold text-[1.5rem] mb-2 text-[#EDE9FE]">
-                                        Product
-                                    </h3>
-                                    <a className="block mt-2 link link-hover">Overview</a>
-                                    <a className="block mt-2 link link-hover">Features</a>
-                                    <a className="block mt-2 link link-hover">Tutorials</a>
-                                    <a className="block mt-2 link link-hover">Pricing</a>
-                                    <a className="block mt-2 link link-hover">Releases</a>
-                                </div>
-
-                                <div>
-                                    <h3 className="font-semibold text-[1.5rem] mb-2 text-[#EDE9FE]">
-                                        Company
-                                    </h3>
-                                    <a className="block mt-2 link link-hover">About</a>
-                                    <a className="block mt-2 link link-hover">Press</a>
-                                    <a className="block mt-2 link link-hover">Careers</a>
-                                    <a className="block mt-2 link link-hover">Contact</a>
-                                    <a className="block mt-2 link link-hover">Partners</a>
-                                </div>
-
-                                <div>
-                                    <h3 className="font-semibold text-[1.5rem] mb-2 text-[#EDE9FE]">
-                                        Support
-                                    </h3>
-                                    <a className="block mt-2 link link-hover">Help Center</a>
-                                    <a className="block mt-2 link link-hover">Terms of Service</a>
-                                    <a className="block mt-2 link link-hover">Legal</a>
-                                    <a className="block mt-2 link link-hover">Privacy Policy</a>
-                                    <a className="block mt-2 link link-hover">Status</a>
-                                </div>
-                                <div>
-                                    <h3 className="font-semibold text-[1.5rem] mb-2 text-[#EDE9FE]">
-                                        Follow Us
-                                    </h3>
-                                    <a className="block mt-2 link link-hover">Facebook</a>
-                                    <a className="block mt-2 link link-hover">Twitter</a>
-                                    <a className="block mt-2 link link-hover">Dribble</a>
-                                    <a className="block mt-2 link link-hover">Instagram</a>
-                                    <a className="block mt-2 link link-hover">LinkedIn</a>
-                                </div>
-
-                            </div>
-                        </div>
+            <div className="bg-[#1b1b1b] h-4"></div>
+            <footer className="bg-[#0C0C0C] text-[#E5E7EB] py-10 laptop:px-12 tablet:px-8 px-4 bottom-0">
+                <div className="w-full laptop:flex space-y-2 laptop:space-y-0 justify-between py-2 border-y border-white/20">
+                    <div className="flex laptop:gap-8 gap-2 justify-center">
+                        <p>
+                            Follow Us
+                        </p>
+                        <p>
+                            Facebook
+                        </p>
+                        <p>
+                            Twitter
+                        </p>
+                        <p>
+                            Instagram
+                        </p>
+                    </div>
+                    <div className="flex justify-center text-center">
+                        <p className="text-sm">
+                            © Copyright <span className="text-sm">{theDate}</span> by ELS. Design by <a className="text-[#FDB913] link link-hover text-sm">Temsdesgn</a> with love
+                            {/* All rights reserved */}
+                        </p>
                     </div>
                 </div>
             </footer>
--- src/Components/Navbar/NavbarData.jsx
+++ src/Components/Navbar/NavbarData.jsx
@@ -22,27 +22,3 @@ export const NavbarData = [
   },
 
 ];
-
-export const FooterData = [
-  {
-    header: "Product",
-    // links: ["Overview", "Features", "Tutorials", "Pricing", "Releases"],
-    // linksTo: ["Overview", "Features", "Tutorials", "Pricing", "Releases"],
-  },
-  {
-    header: "Company",
-    // links: ["About", "Press", "Careers", "Contact", "Partners"],
-    // linkTo: ["About", "Press", "Careers", "Contact", "Partners"],
-  },
-  {
-    header: "Support",
-    // links: ["Help Center", "Terms of service", "Legal", "Privacy Policy", "Status"],
-    // linkTo: ["Help Center", "Terms of service", "Legal", "Privacy Policy", "Status"],
-  },
-  {
-    header: "Follow us",
-    // links: ["Facebook", "Twitter", "Dribble", "Instagram", "LinkedIn"],
-    // linkTo: ["Facebook", "Twitter", "Dribble", "Instagram", "LinkedIn"],
-  },
-
-];
