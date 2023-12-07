ecfb9d99da220093043b556cf9a711d2dcbce7fc
Stephen Adeniji <praiseadeniji2017@gmail.com>
2023-12-07 14:51:22 +0100

    feat: video bg and responsiveness

--- src/Pages/Home/Header.jsx
+++ src/Pages/Home/Header.jsx
@@ -1,47 +1,41 @@
-// import { TypeAnimation } from 'react-type-animation';
-
-import Sedan from "../../assets/sedann.png"
-import SUVs from "../../assets/suvv.png"
-import Coupe from "../../assets/ccoupe.png"
-import Helicopter from "../../assets/helicopterr.png"
+import Sedan from "../../assets/sedan.png"
+import SUVs from "../../assets/suv.png"
+import Vans from "../../assets/van.png"
+// import Helicopter from "../../assets/helicopterr.png"
 import ColorBlocks from "../../assets/color blocks.png"
 // import Bentley from "../../assets/hero-bg.png"
 import { Link } from "react-router-dom"
+import Video from "../../assets/ELS.mp4"
 
 const Header = () => {
     return (
         <div>
-            {/* <section className="bg-hero-bg bg-auto bg-custom"> */}
-            <section className="relative pt-20 tablet:bg-hero-bg bg-cover min-h-screen bg-center">
-                {/* <div className="grid max-w-screen-xl px-4 py-8 bg-hero-bg bg-opacity-10 bg-cover opacity-30 mx-auto laptop:gap-8 xl:gap-0 laptop:py-16 laptop:grid-cols-12"> */}
-
+            {/* <section className="relative pt-20 tablet:bg-hero-bg bg-cover min-h-screen bg-center"> */}
+            <section className="relative pt-20 h-screen">
+                {/* video bg */}
+                <div className="absolute inset-0 z-0">
+                    <video
+                        className="w-full h-screen object-cover"
+                        autoPlay
+                        loop
+                        muted
+                        playsInline
+                    >
+                        <source src={Video} type="video/mp4" />
+                        Your browser does not support the video tag.
+                    </video>
+                </div>
                 {/* Overlay */}
-                {/* disabled because it covers the frst part of the header */}
-                {/* <div className="absolute inset-0 bg-black opacity-40 z-0"></div> */}
+                <div className="absolute inset-0 bg-black opacity-40 z-0"></div>
 
-                <div className="grid max-w-screen-xl z-30 laptop:px-12 tablet:px-8 px-4 pt-12 mx-auto laptop:gap-8 laptop:grid-cols-12">
+                {/* intro */}
+                <div className="z-50 laptop:px-12 tablet:px-8 px-4 pt-12 mx-auto">
                     <div className="laptop:col-span-7 w-max backdrop-blur-sm">
                         <img src={ColorBlocks} className='mb-4' alt='color-blocks' />
                         <h1
                             className="mb-5 text-4xl tablet:text-5xl laptop:text-[3.5rem] tracking-tight leading-none font-black">
                             Embark on a journey of <br />
                             opulence
-                            {/* <TypeAnimation
-                                sequence={[
-                                    // Same substring at the start will only be typed once, initially
-                                    'opulence',
-                                    1000,
-                                    'prestige',
-                                    1000,
-                                    'affluence',
-                                    1000,
-                                    'grandeur',
-                                    1000,
-                                ]}
-                                speed={50}
-                                style={{ fontSize: '2.25rem' }}
-                                repeat={Infinity}
-                            /> */}
                         </h1>
                         <p className="max-w-2xl mb-6 font-light laptop:leading-[2rem] text-white laptop:mb-8 tablet:text-lg laptop:text-[1.225rem]">
                             Every Detail Crafted for the Discerning Connoisseur. <br />
@@ -56,45 +50,36 @@ const Header = () => {
                     </div>
                 </div>
 
-                <div className='laptop:mt-12 mt-6 laptop:bg-opacity-10 grid laptop:backdrop-blur-sm px-6 tablet:px-8 laptop:px-12 py-6 rounded-lg bg-transparent laptop:grid-cols-4 grid-cols-2 laptop:gap-8 gap-10 justify-between'>
+                {/* categories */}
+                <div className='laptop:mt-10 tablet:mt-20 z-30 mt-6 grid backdrop-blur-sm px-6 tablet:px-8 laptop:px-12 py-6 rounded-lg bg-transparent tablet:grid-cols-3 grid-cols-2 laptop:gap-8 gap-10 mx-auto'>
                     <span>
-                        <img src={Sedan} className='w-[10%]' alt='sedan' />
+                        <img src={Sedan} className='w-[13%]' alt='sedan' />
                         <h2 className='text-xl font-semibold'>
                             Sedans
                         </h2>
-                        <p className='text-base'>
-                            Mercedes-Benz S-Class, <br />
-                            BMW 7 Series, Audi A8
+                        <p className='text-sm'>
+                            TOYOTA COROLLA 2014 - 2016, <br />
+                            TOYOTA COROLLA 2017 - 2018
                         </p>
                     </span>
                     <span>
-                        <img src={SUVs} className='w-[10%]' alt='sedan' />
+                        <img src={SUVs} className='w-[13%]' alt='SUV' />
                         <h2 className='text-xl font-semibold'>
                             SUVs
                         </h2>
-                        <p className='text-base'>
-                            Range Rover,Bentley <br />
-                            Bentayga,Rolls-RoyceCullinan
+                        <p className='text-sm'>
+                            TOYOTA RAV 4 2019, <br />
+                            LEXUS GX 460 2015
                         </p>
                     </span>
                     <span>
-                        <img src={Coupe} className='w-[10%]' alt='sedan' />
+                        <img src={Vans} className='w-[13%]' alt='van' />
                         <h2 className='text-xl font-semibold'>
-                            Coupe
+                            Vans
                         </h2>
-                        <p className='text-base'>
-                            Bentley Continental GT <br />
-                            Mercedes-Benz S-Class
-                        </p>
-                    </span>
-                    <span>
-                        <img src={Helicopter} className='w-[10%]' alt='sedan' />
-                        <h2 className='text-xl font-semibold'>
-                            Helicopter
-                        </h2>
-                        <p className='text-base'>
-                            Robinson R44 <br />
-                            Bell 206 JetRanger
+                        <p className='text-sm'>
+                            TOYOTA HIACE 2018, <br />
+                            TOYOTA COASTER
                         </p>
                     </span>
                 </div>
