ddfad58a59f9725f47061017b5d3b7bd508cf925
Stephen Adeniji <praiseadeniji2017@gmail.com>
2023-12-08 19:38:31 +0100

    Changed 'explore' button to 'book now'

--- src/Pages/Home/Header.jsx
+++ src/Pages/Home/Header.jsx
@@ -42,7 +42,7 @@ const Header = () => {
                         </p>
                         <Link to="booking">
                             <button className="inline-flex items-center justify-center px-9 py-4 border border-transparent rounded-3xl font-semibold bg-[#FEBB1B] text-black hover:border hover:border-[#FEBB1B] hover:text-white hover:bg-transparent focus:ring-1 focus:ring-[#FEBB1B] ">
-                                Explore
+                                Book Now
                             </button>
                         </Link>
                     </div>
@@ -73,7 +73,7 @@ const Header = () => {
                     <span>
                         <img src={Vans} className='w-[13%]' alt='van' />
                         <h2 className='text-xl font-semibold'>
-                            Vans
+                            Toyota
                         </h2>
                         <p className='text-sm'>
                             TOYOTA HIACE 2018, <br />
