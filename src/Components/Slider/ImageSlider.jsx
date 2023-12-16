163ad877c93fe25f26154fc4352eb25d6d829b33
Stephen Adeniji <praiseadeniji2017@gmail.com>
2023-12-16 12:56:17 +0100

    changed 'fleet' cars images and colors

--- src/Components/Slider/ImageSlider.jsx
+++ src/Components/Slider/ImageSlider.jsx
@@ -6,8 +6,8 @@ const ImageSlider = () => {
     return (
         <div className="h-80 tablet:hidden">
             <Carousel slideInterval={3000}>
-                <img src="https://img.sbtjapan.com/dealercarphoto/DTS4562/f.jpg?v=1691747082&imwidth=640" className="opacity-60" alt="TOYOTA FORTUNER 2019" />
-                <img src="https://senda.us/autocraft/avisnew/images/veh_images/1611909034_image_13.jpeg" className="opacity-60" alt="TOYOTA LANDCRUISER 2018" />
+                <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSTROPkHgDyJkGDf_DKpY8fxXTd2YmKnvWHag&usqp=CAU" className="opacity-60" alt="TOYOTA FORTUNER 2019" />
+                <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSMmKRwgLH-3Ms0ari546CIGj9eWHYTRSVkSg8DIY8LWjqMJOCCoHcD4LQUQaD5aDUS6KE&usqp=CAU" className="opacity-60" alt="TOYOTA LANDCRUISER VX" />
                 <img src="https://senda.us/autocraft/avisnew/images/veh_images/16135224914image_13.jpeg" className="opacity-60" alt="TOYOTA PRADO 2018" />
                 <img src="https://wehco.media.clients.ellingtoncms.com/timesfreepress/img/photos/2014/02/28/0301_WEB_c_Test_Drive.JPG" className="opacity-60" alt="LEXUS GX 460 2015" />
             </Carousel>
--- src/Pages/Home/BecomeFleetOwner.jsx
+++ src/Pages/Home/BecomeFleetOwner.jsx
@@ -12,8 +12,8 @@ const BecomeFleetOwner = () => {
                 </h1>
 
                 <div className="tablet:grid hidden laptop:grid-cols-4 tablet:grid-cols-2 grid-cols-1 gap-10">
-                    <img src="https://img.sbtjapan.com/dealercarphoto/DTS4562/f.jpg?v=1691747082&imwidth=640" className="w-fit mx-auto opacity-60 rounded-xl" alt="TOYOTA FORTUNER 2019" />
-                    <img src="https://senda.us/autocraft/avisnew/images/veh_images/1611909034_image_13.jpeg" className="w-fit mx-auto opacity-60 rounded-xl" alt="TOYOTA LANDCRUISER 2018" />
+                    <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSTROPkHgDyJkGDf_DKpY8fxXTd2YmKnvWHag&usqp=CAU" className="w-fit mx-auto opacity-60 rounded-xl" alt="TOYOTA FORTUNER 2019" />
+                    <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSMmKRwgLH-3Ms0ari546CIGj9eWHYTRSVkSg8DIY8LWjqMJOCCoHcD4LQUQaD5aDUS6KE&usqp=CAU" className="w-fit mx-auto opacity-60 rounded-xl" alt="TOYOTA LANDCRUISER VX" />
                     <img src="https://senda.us/autocraft/avisnew/images/veh_images/16135224914image_13.jpeg" className="w-fit mx-auto opacity-60 rounded-xl" alt="TOYOTA PRADO 2018" />
                     <img src="https://wehco.media.clients.ellingtoncms.com/timesfreepress/img/photos/2014/02/28/0301_WEB_c_Test_Drive.JPG" className="w-fit mx-auto opacity-60 rounded-xl" alt="LEXUS GX 460 2015" />
                 </div>
