c78d6126f2131f43d5acb3af10be1af3525470c7
Stephen Adeniji <praiseadeniji2017@gmail.com>
2023-12-17 22:23:53 +0100

    Resized 'fleet' images

--- src/Pages/Home/BecomeFleetOwner.jsx
+++ src/Pages/Home/BecomeFleetOwner.jsx
@@ -1,4 +1,8 @@
 import ImageSlider from "../../Components/Slider/ImageSlider"
+import Fortuner from "../../assets/fortuner.jpeg"
+import Landcruiser from "../../assets/landcruiser.jpeg"
+import Prado from "../../assets/prado.jpeg"
+import Lexus from "../../assets/lexus.jpeg"
 
 
 
@@ -12,10 +16,10 @@ const BecomeFleetOwner = () => {
                 </h1>
 
                 <div className="tablet:grid hidden laptop:grid-cols-4 tablet:grid-cols-2 grid-cols-1 gap-10">
-                    <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSTROPkHgDyJkGDf_DKpY8fxXTd2YmKnvWHag&usqp=CAU" className="w-fit mx-auto opacity-60 rounded-xl" alt="TOYOTA FORTUNER 2019" />
-                    <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSMmKRwgLH-3Ms0ari546CIGj9eWHYTRSVkSg8DIY8LWjqMJOCCoHcD4LQUQaD5aDUS6KE&usqp=CAU" className="w-fit mx-auto opacity-60 rounded-xl" alt="TOYOTA LANDCRUISER VX" />
-                    <img src="https://senda.us/autocraft/avisnew/images/veh_images/16135224914image_13.jpeg" className="w-fit mx-auto opacity-60 rounded-xl" alt="TOYOTA PRADO 2018" />
-                    <img src="https://wehco.media.clients.ellingtoncms.com/timesfreepress/img/photos/2014/02/28/0301_WEB_c_Test_Drive.JPG" className="w-fit mx-auto opacity-60 rounded-xl" alt="LEXUS GX 460 2015" />
+                    <img src={Fortuner} className="w-fit mx-auto opacity-60 rounded-xl" alt="TOYOTA FORTUNER 2019" />
+                    <img src={Landcruiser} className="w-fit mx-auto opacity-60 rounded-xl" alt="TOYOTA LANDCRUISER VX" />
+                    <img src={Prado} className="w-fit mx-auto opacity-60 rounded-xl" alt="TOYOTA PRADO 2018" />
+                    <img src={Lexus} className="w-fit mx-auto opacity-60 rounded-xl" alt="LEXUS GX 460 2015" />
                 </div>
 
                 <ImageSlider />
